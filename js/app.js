545ee411e25efcf13d669c90d13a33c6f614f5b8
EvertonOliveira031 <everton.31.oliveira@gmail.com>
2024-01-05 19:36:39 -0300

    primeira alteração no arquivo js

---
+++ js/app.js
@@ -0,0 +1,14 @@
+function adicionar(){
+    //recuperar valores nome, quantidade e valor do produto
+    //calcular o preço; o subtotal;
+    //colocar no carrinho
+    //atualizar o valor total
+
+    let nomeProduto = document.getElementById('produto');
+    let quantidade = document.getElementById('quantidade')
+    
+}
+
+function limpar() {
+    
+}
